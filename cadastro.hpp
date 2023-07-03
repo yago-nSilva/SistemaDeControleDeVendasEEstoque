4883582a45217516d8433fdd9aaaeaaba3d61e38
yago-nSilva <133616288+yago-nSilva@users.noreply.github.com>
2023-07-03 15:56:30 -0300

    Update cadastro.hpp

--- cadastro.hpp
+++ cadastro.hpp
@@ -1,5 +1,5 @@
-#ifndef EMPRESA_H
-#define EMPRESA_H
+#ifndef CADASTRO_H
+#define CADASTRO_H
 
 #include <iostream>
 #include <vector>
@@ -21,6 +21,6 @@ class Cadastro{
       std::vector<std::string> Password;
 };
 
-#include "empresa_.cpp"
+#include "cadastro_.cpp"
 
 #endif
