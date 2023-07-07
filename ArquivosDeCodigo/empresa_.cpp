5049830d04f8ef46f59b9edd1719558f4a024771
yago-nSilva <133616288+yago-nSilva@users.noreply.github.com>
2023-07-06 22:56:44 -0300

    Update empresa_.cpp

--- ArquivosDeCodigo/empresa_.cpp
+++ ArquivosDeCodigo/empresa_.cpp
@@ -16,38 +16,35 @@ void Cadastro::insertUsername(std::string username){
     Username.push_back(username);
 }
 
-void Cadastro::forcedinsertUsername(std::string username){
-    Username.back() = username;
-}
-
-// bool Cadastro::verifyUsername(std::string username){
-//     if (Username.empty()) return true;
-
-//     unsigned test_key = 0;
-//     for (unsigned i = 0; i < Username.size() - 1; i++){
-        
-//         if (username == Username.at(i)) test_key++;
-//         if (test_key > 1) {
-//             return false;
-//         }
-//     }
-
-//     return true;
-// }
-
 void Cadastro::insertPassword(std::string password){
     Password.push_back(password);
 }
 
-bool Cadastro::confirmPassword(std::string password){
-  //  if (Password.empty()) return false;
+bool Cadastro::confirmPassword(std::string confirmation_password){
 
-    for (unsigned i = 0; i < Password.size(); i++){
-        if (password == Password.at(i)) return true;
-    }
+  if (confirmation_password == Password.back()) return true;
 
-      Username.erase(Username.end() - 1);
-      Password.erase(Password.end() - 1);
-
-    return false;
+  return false;
 }
+
+void Cadastro::deleteUsername(){
+  Username.erase(Username.end() - 1);
+}
+
+void Cadastro::deletePassword(){
+  Password.erase(Password.end() - 1);
+}
+
+std::string FileAux::turnintoNamefile(std::string name){
+  
+  std::string namefile;
+
+  namefile = name + ".txt";
+
+  return name;
+}
+
+bool LoginFile::isfileOpen(std::string const namefile){
+  return _file.good();
+}
+
